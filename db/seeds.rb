5f741c9e7e1351fe5b018ae4939275ac72eee0ee
baptiste giorgi <badbat_69@hotmail.com>
2021-12-08 17:00:19 +0100

    firsts steps on simple-airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do
+  Flat.create!(
+    name: Faker::ChuckNorris.fact,
+    address: Faker::Address.full_address,
+    description: Faker::Movies::VForVendetta.speech,
+    price_per_night: rand(150),
+    number_of_guests: rand(5)
+  )
+end
